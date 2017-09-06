13108e539f6fe91f2f73fac32048c70ea3f8e843
rymuff <rymuff@gmail.com>
2017-09-06 13:30:49 +0900

    Update

--- src/com/kweisa/Client.java
+++ src/com/kweisa/Client.java
@@ -5,18 +5,17 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.security.*;
-import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.Security;
 
 
 public class Client {
     private String serverAddress;
     private int port;
     private Certificate certificate;
-    private PrivateKey privateKey;
 
     public Client(String serverAddress, int port) {
         this.serverAddress = serverAddress;
@@ -24,15 +23,7 @@ public class Client {
     }
 
     public void load(String certificateFileName, String privateKeyFileName) throws Exception {
-        Security.addProvider(new BouncyCastleProvider());
-        certificate = Certificate.read(certificateFileName);
-
-        FileInputStream fileInputStream = new FileInputStream(privateKeyFileName);
-        byte[] bytes = new byte[fileInputStream.available()];
-        fileInputStream.read(bytes);
-        fileInputStream.close();
-
-        privateKey = KeyFactory.getInstance("EC").generatePrivate(new PKCS8EncodedKeySpec(bytes));
+        certificate = Certificate.read(certificateFileName, privateKeyFileName);
     }
 
     public void connect() throws IOException, NoSuchAlgorithmException {
@@ -69,14 +60,9 @@ public class Client {
         return bytes;
     }
 
-    public static String byteArrayToHex(byte[] a) {
-        StringBuilder sb = new StringBuilder(a.length * 2);
-        for (byte b : a)
-            sb.append(String.format("%02x", b));
-        return sb.toString();
-    }
-
     public static void main(String[] args) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+
         Client client = new Client("127.0.0.1", 10002);
         client.load("client.cert", "client.key");
         client.connect();
